ae631b6bcd00a0b8ab86f287fe7d0ed68f0452f6
MatteoZattini <matteozattini@hotmail.com>
2024-06-13 14:57:01 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        toDoList: []
+    data() {
+        return {
+            toDoList: [],
+            myInput: "",
+        }
+    },
+    methods: {
+        addElement() {
+            console.log(this.myInput)
+            this.toDoList.push({"titolo":this.myInput, "done": true})
+            this.myInput = ""
+            console.log(this.toDoList)
+        },
+    },
+    mounted() {
+        console.log("Recupero i dati dal server");
+
+        axios.get("./server.php").then(results => {
+            console.log("Risultati: ", results);
+            this.toDoList = results.data;
+        });
     }
-  },
-  methods: {
-
-  },
-  mounted() {
-    console.log("Recupero i dati dal server");
-
-    axios.get("./server.php").then(results => {
-        console.log("Risultati: ", results);
-        this.toDoList = results.data;
-    });
-  }
 }).mount('#app')
